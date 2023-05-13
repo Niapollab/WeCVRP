2cdaf0cc5a91bfa961cafb906d4465f95be2b0f3
Niapollab <87022299+Niapollab@users.noreply.github.com>
2023-05-13 18:37:28 +0400

    Initial commit

---
+++ src/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeCVRP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
