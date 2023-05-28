8f72ac6d62bda5f4b3005b9ee25e2f331f08a1d5
Niapoll <Kolya21082001@gmail.com>
2023-05-28 22:23:44 +0400

    Move logic from MainPageViewModel to MainPage

--- src/WeCVRP.UI/ViewModels/MainPageViewModel.cs
+++ src/WeCVRP.UI/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using WeCVRP.UI.Models;
-using WeCVRP.UI.Extensions;
-using Map = Mapsui.Map;
-using Mapsui;
-using WeCVRP.UI.Controllers;
 
 namespace WeCVRP.UI.ViewModels;
 
@@ -23,47 +19,7 @@ public partial class MainPageViewModel : ObservableObject
     private string? _placeName;
 
     [ObservableProperty]
-    private Map _map;
-
-    [ObservableProperty]
-    public bool _hasFoundLocations;
-
-    private Location _homeLocation;
-
-    private readonly SearchLocationController _searchLocationController;
-
-    public MainPageViewModel()
-    {
-        _searchLocationController = new SearchLocationController();
-        _map = new Map();
-
-        // Location shown on map by default (if location service is not available)
-        _homeLocation = new Location(20.798363, -156.331924);
-
-        _map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
-        _map.Home = n => n.CenterOnAndZoomTo(_homeLocation.ToMPoint(), n.Resolutions[10]);
-    }
-
-    [RelayCommand]
-    private static Task OpenFileAsync()
-        => Task.CompletedTask;
-
-    [RelayCommand]
-    private static Task SaveFileAsync()
-        => Task.CompletedTask;
-
-    [RelayCommand]
-    private static Task SaveAsFileAsync()
-        => Task.CompletedTask;
-
-    [RelayCommand]
-    private static void OpenAbout()
-    {
-    }
-
-    [RelayCommand]
-    private static void Exit()
-        => Environment.Exit(0);
+    private bool _hasFoundLocations;
 
     [RelayCommand]
     private void AlgorithmClicked(string? value)
@@ -72,60 +28,4 @@ public partial class MainPageViewModel : ObservableObject
     [RelayCommand]
     private void DirectionClicked(string? value)
         => Direction = Enum.Parse<Direction>(value!);
-
-    [RelayCommand]
-    private async Task SearchAsync()
-    {
-        if (string.IsNullOrEmpty(PlaceName))
-        {
-            HasFoundLocations = false;
-            return;
-        }
-
-        Location? location = _searchLocationController.GetNextLocation();
-
-        if (location is null)
-        {
-            await _searchLocationController.TryUpdateAsync(PlaceName);
-            location = _searchLocationController.GetNextLocation();
-
-            HasFoundLocations = location is not null;
-
-            if (location is null)
-                return;
-        }
-
-        Navigator navigator = Map.Navigator;
-        navigator.CenterOnAndZoomTo(location.ToMPoint(), Map.Navigator.Resolutions[16]);
-    }
-
-    public void DropLocations()
-    {
-        HasFoundLocations = false;
-        _searchLocationController.Clear();
-    }
-
-    public async ValueTask<bool> TryToMoveToUserLocationAsync(double? resolution = null, CancellationToken cancellationToken = default)
-    {
-        resolution ??= Map.Navigator.Resolutions[16];
-        try
-        {
-            Location? location = await Geolocation.Default.GetLastKnownLocationAsync()
-                ?? await Geolocation.Default.GetLocationAsync(new GeolocationRequest(), cancellationToken);
-
-            if (location == null)
-                return false;
-
-            Navigator navigator = Map.Navigator;
-            navigator.CenterOnAndZoomTo(location.ToMPoint(), resolution.Value);
-
-            _homeLocation = location;
-
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
 }
--- src/WeCVRP.UI/Views/MainPage.xaml.cs
+++ src/WeCVRP.UI/Views/MainPage.xaml.cs
@@ -1,19 +1,113 @@
-﻿namespace WeCVRP.UI.Views;
+﻿using Mapsui;
+using WeCVRP.UI.Controllers;
+using WeCVRP.UI.Extensions;
+
+namespace WeCVRP.UI.Views;
 
 public partial class MainPage : ContentPage
 {
+    private Location _homeLocation;
+
+    private readonly SearchLocationController _searchLocationController;
+
     public MainPage()
     {
         InitializeComponent();
+
+        // Location shown on map by default (if location service is not available)
+        _homeLocation = new Location(20.798363, -156.331924);
+
+        _searchLocationController = new SearchLocationController();
         mapView.MyLocationLayer.Enabled = false;
+
+        mapView.Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        mapView.Map.Home = n => n.CenterOnAndZoomTo(_homeLocation.ToMPoint(), n.Resolutions[10]);
     }
 
     protected async override void OnAppearing()
     {
         await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-        await mainPageViewModel.TryToMoveToUserLocationAsync();
+        await TryToMoveToUserLocationAsync();
     }
 
     private void OnSearchTextChanged(object? sender, EventArgs eventArgs)
-        => mainPageViewModel.DropLocations();
+        => DropLocations();
+
+    private void OnOpenFileClicked(object sender, EventArgs eventArgs)
+    {
+        // TODO
+    }
+
+    private void OnSaveFileClicked(object sender, EventArgs eventArgs)
+    {
+        // TODO
+    }
+
+    private void OnSaveAsFileClicked(object sender, EventArgs eventArgs)
+    {
+        // TODO
+    }
+
+    private void OnOpenAboutClicked(object sender, EventArgs eventArgs)
+    {
+        // TODO
+    }
+
+    private void OnExitClicked(object sender, EventArgs eventArgs)
+        => Environment.Exit(0);
+
+    private async void OnSearch(object sender, EventArgs eventArgs)
+    {
+        if (string.IsNullOrEmpty(mainPageViewModel.PlaceName))
+        {
+            mainPageViewModel.HasFoundLocations = false;
+            return;
+        }
+
+        Location? location = _searchLocationController.GetNextLocation();
+
+        if (location is null)
+        {
+            await _searchLocationController.TryUpdateAsync(mainPageViewModel.PlaceName);
+            location = _searchLocationController.GetNextLocation();
+
+            mainPageViewModel.HasFoundLocations = location is not null;
+
+            if (location is null)
+                return;
+        }
+
+        Navigator navigator = mapView.Map.Navigator;
+        navigator.CenterOnAndZoomTo(location.ToMPoint(), mapView.Map.Navigator.Resolutions[16]);
+    }
+
+    private void DropLocations()
+    {
+        mainPageViewModel.HasFoundLocations = false;
+        _searchLocationController.Clear();
+    }
+
+    private async ValueTask<bool> TryToMoveToUserLocationAsync(double? resolution = null, CancellationToken cancellationToken = default)
+    {
+        resolution ??= mapView.Map.Navigator.Resolutions[16];
+        try
+        {
+            Location? location = await Geolocation.Default.GetLastKnownLocationAsync()
+                ?? await Geolocation.Default.GetLocationAsync(new GeolocationRequest(), cancellationToken);
+
+            if (location == null)
+                return false;
+
+            Navigator navigator = mapView.Map.Navigator;
+            navigator.CenterOnAndZoomTo(location.ToMPoint(), resolution.Value);
+
+            _homeLocation = location;
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
